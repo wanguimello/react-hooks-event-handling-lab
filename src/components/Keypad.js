de986a51a439e8aa8ef5a5c7dba8ee2f48aec53c
joycewangui <joycewangui590@gmail.com>
2024-08-22 09:01:11 +0300

    Event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
 
+import React from 'react';
+
 function Keypad (){
+    function handleChange(event) {
+        event.preventDefault()
+        console.log('Entering password...');
+    };
     return (
-        <div></div>
-    )
+        <input type="password" onChange={handleChange} />   
+    );
 }
 
 export default Keypad;
